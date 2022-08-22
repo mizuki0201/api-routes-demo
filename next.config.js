62e8a7a46e93f228db17777a19e2cfb56f008a44
mizuki0201 <6m29uv7.tankss@gmail.com>
2022-08-22 10:03:26 +0900

    feat: next.configのrewrites設定

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [
+      // クーポンURLでアクセスがあった場合はAPI Routesの処理に飛ばすように設定しています
+      {
+        source: "/coupon/:userId",
+        destination: "/api/coupon/:userId",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
